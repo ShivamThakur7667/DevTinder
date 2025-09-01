02eb25b3d9b33cbade2bc315c88a9fafe9aa489b
ShivamThakur7667 <shivamthakur7667@gmail.com>
2025-09-01 11:26:34 +0530

    playing with the routes

--- src/app.js
+++ src/app.js
@@ -2,22 +2,13 @@ const express = require("express");
 
 const app = express();
 
-// this will only handle GET call to /user
-app.get("/user", (req, res) => {
+
+app.get("/user/:userId/:name/:password", (req, res) => {
+  console.log(req.params);
   res.send({ firstname: "shivam", lastname: "thakur" });
 });
 
-app.post("/user", async (req, res) => {
-  console.log(req.body);
 
-  // saving data to db
-  res.send("Data successfully saved to the database !");
-});
-
-// this will match all the HTTP method API calls to /test
-app.delete("/user", (req, res) => {
-  res.send("Hello from the server !");
-});
 
 app.listen(3000, (req, res) => {
   console.log("server is listing on port 3000");
