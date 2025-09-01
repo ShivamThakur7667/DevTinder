ddf4bd62ee1a95c3d38d35c5428287188e4e9eff
ShivamThakur7667 <shivamthakur7667@gmail.com>
2025-09-01 11:17:34 +0530

    exploring get, post, delete

--- homework.md
+++ homework.md
@@ -11,3 +11,14 @@ EXPRESS SERVER ->
 -> what are dependencies
 -> what is the use "-g" while npm install
 -> Difference between (^ and ~)
+
+ROUTING & REQUEST HANDLERS ->
+
+-> Initialize git 
+-> .gitignore
+-> Create a remote repo on github
+-> Push all code to remote origin
+-> Play with routes and route extensions ex. /hello, /, hello/2, /xyz
+-> Order of the routes matter a lot
+-> Install Postman app and make a workspace/collection > test API call
+-> Write logic to handle GET, POST, PATCH, DELETE API Calls and test them on Postman
--- src/app.js
+++ src/app.js
@@ -2,16 +2,22 @@ const express = require("express");
 
 const app = express();
 
-app.use("/", (req, res) =>{
-    res.send("Hi, I am Shivam");
-})
+// this will only handle GET call to /user
+app.get("/user", (req, res) => {
+  res.send({ firstname: "shivam", lastname: "thakur" });
+});
 
+app.post("/user", async (req, res) => {
+  console.log(req.body);
 
+  // saving data to db
+  res.send("Data successfully saved to the database !");
+});
 
-
-
-
-
+// this will match all the HTTP method API calls to /test
+app.delete("/user", (req, res) => {
+  res.send("Hello from the server !");
+});
 
 app.listen(3000, (req, res) => {
   console.log("server is listing on port 3000");
