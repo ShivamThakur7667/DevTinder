10ad7faf0622911b03fffc54636d47db2a173547
ShivamThakur7667 <shivamthakur766793@gmail.com>
2025-09-16 13:44:40 +0530

    Using Cors in the backend and also send the user info to frontend when a user login from frontend

--- src/app.js
+++ src/app.js
@@ -2,10 +2,16 @@ const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
-
 
 const authRouter = require("./routes/auth");
 const profileRouter = require("./routes/profile");
@@ -16,7 +22,6 @@ app.use("/", authRouter);
 app.use("/", profileRouter);
 app.use("/", requestRouter);
 app.use("/", userRouter);
-
 
 connectDB()
   .then(() => {
