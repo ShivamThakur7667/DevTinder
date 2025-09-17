da7343d0765fe2a2d2eb6d4760853aa14493df79
ShivamThakur7667 <shivamthakur766793@gmail.com>
2025-09-17 11:01:29 +0530

    Updated the error message for user when not Logged-In

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -7,14 +7,14 @@ const userAuth = async (req, res, next) => {
   try {
     const cookies = req.cookies;
     const { token } = cookies;
-    if(!token){
-      throw new Error("Token is not valid!!");
+    if (!token) {
+      return res.status(401).send("Please LogIn");
     }
 
     const decodedObj = jwt.verify(token, JWT_SECRET);
 
     const { _id } = decodedObj;
-    
+
     const user = await User.findById(_id);
 
     if (!user) {
@@ -22,7 +22,6 @@ const userAuth = async (req, res, next) => {
     }
     req.user = user;
     next();
-
   } catch (error) {
     res.status(400).send(" ERROR : " + error.message);
   }
